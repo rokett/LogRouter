d9e08c215bf9f1e34c6c3c192a86acb09b0b283e
Rokett <rokett@rokett.me>
2020-05-16 09:54:57 +0100

    Migrated from dep to Go modules

--- make.bat
+++ make.bat
@@ -1,8 +1,6 @@
 @echo off
 SETLOCAL
 
-set _TARGETS=build
-
 set APP=LogRouter
 set VERSION=1.0.0
 set BINARY-X86=%APP%_%VERSION%_Windows_32bit.exe
@@ -11,13 +9,9 @@ set BINARY-X64=%APP%_%VERSION%_Windows_64bit.exe
 REM Set build number from git commit hash
 for /f %%i in ('git rev-parse HEAD') do set BUILD=%%i
 
-if [%1]==[] goto usage
+set LDFLAGS=-ldflags "-X main.version=%VERSION% -X main.build=%BUILD% -s -w -extldflags '-static'"
 
-REM *** CHECK THAT VALID ARG IS PASSED ***
-
-set LDFLAGS=-ldflags "-X main.version=%VERSION% -X main.build=%BUILD%"
-
-goto %1
+goto build
 
 :build
     set GOOS=windows
@@ -25,21 +19,14 @@ goto %1
     echo "=== Building x86 ==="
     set GOARCH=386
 
-    go build -o %BINARY-X86% %LDFLAGS%
+    go build -mod=vendor -o %BINARY-X86% %LDFLAGS%
 
     echo "=== Building x64 ==="
     set GOARCH=amd64
 
-    go build -o %BINARY-X64% %LDFLAGS%
+    go build -mod=vendor -o %BINARY-X64% %LDFLAGS%
 
     goto :finalise
-
-:usage
-	echo usage: make [target]
-	echo.
-	echo target is one of {%_TARGETS%}.
-	exit /b 2
-	goto :eof
 
 :finalise
     set GOOS=
